fa97c06434db1e76539a58fe91bbb9778083387d
priyanshu <priyanshukumari93933@gmail.com>
2025-11-26 19:52:02 +0530

    Responsive Website with Navigation

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div className="flex h-[91vh] items-center justify-center bg-slate-800 font-sans ">
+      <main className="flex  w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-slate-600   rounded-xl">
+        <h1>Create a responsive website using Next.js and TypeScript that includes a functional navigation bar with
+          four main sections: Home, About, Services, and Contact. </h1>
+      </main>
+    </div>
+  );
+}
